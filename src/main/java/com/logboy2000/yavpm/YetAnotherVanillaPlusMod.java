508b71384399a4fc76cbfc94d1f1411b74c40e8e
Logan Howarth <loganhowarth@icloud.com>
2023-07-26 00:00:41 -0600

    Egg related food items and /sethome command

--- src/main/java/com/logboy2000/yavpm/YetAnotherVanillaPlusMod.java
+++ src/main/java/com/logboy2000/yavpm/YetAnotherVanillaPlusMod.java
@@ -1,18 +1,34 @@
 package com.logboy2000.yavpm;
 
+import com.logboy2000.yavpm.block.ModBlocks;
+import com.logboy2000.yavpm.command.HomeCommand;
+import com.logboy2000.yavpm.command.SetHomeCommand;
+import com.logboy2000.yavpm.item.ModItemGroups;
+import com.logboy2000.yavpm.item.ModItems;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class YetAnotherVanillaPlusMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("yavpm");
+	public static final String MOD_ID = "yavpm";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
+
+
+		// Register the commands
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			SetHomeCommand.register(dispatcher);
+			HomeCommand.register(dispatcher);
+		});
+
 
 
 	LOGGER.info("Hello Fabric world!");
